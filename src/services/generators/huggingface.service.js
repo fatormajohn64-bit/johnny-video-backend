486ea5c7d6e0dca1af5c8662edca56dd177fee34
Johnny-Tech <fatormajohn64@gmail.com>
2026-09-10 03:58:45 +0000

    Create huggingface.service.js

---
+++ src/services/generators/huggingface.service.js
@@ -0,0 +1,46 @@
+import { InferenceClient } from "@huggingface/inference";
+
+import { env } from "../../config/env.js";
+
+import {
+  HttpError
+} from "../../utils/http-error.js";
+
+export async function generateHuggingFaceVideo({
+  prompt,
+  model,
+  input = {}
+}) {
+  if (!env.hfToken) {
+    throw new HttpError(
+      503,
+      "HF_TOKEN is not configured",
+      "PROVIDER_NOT_CONFIGURED"
+    );
+  }
+
+  const client =
+    new InferenceClient(env.hfToken);
+
+  const selectedModel =
+    model || env.hfVideoModel;
+
+  const video =
+    await client.textToVideo({
+      model: selectedModel,
+      inputs: prompt,
+      ...input
+    });
+
+  return {
+    success: true,
+
+    provider: "huggingface",
+
+    model: selectedModel,
+
+    status: "completed",
+
+    video
+  };
+}
