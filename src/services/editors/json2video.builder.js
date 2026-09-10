32b8fed4953560aeb408ed5de6f74562e26de4c1
Johnny-Tech <fatormajohn64@gmail.com>
2026-09-10 12:46:16 +0000

    Update json2video.builder.js

--- src/services/editors/json2video.builder.js
+++ src/services/editors/json2video.builder.js
@@ -3,7 +3,12 @@ export function buildJson2VideoMovie({
   settings = {}
 }) {
   const movie = {
-    resolution: settings.resolution || "full-hd",
+    resolution:
+      settings.resolution || "full-hd",
+
+    fps:
+      settings.fps || 25,
+
     scenes: [
       {
         elements: [
@@ -16,17 +21,74 @@ export function buildJson2VideoMovie({
     ]
   };
 
+  /*
+  |--------------------------------------------------------------------------
+  | Text overlay
+  |--------------------------------------------------------------------------
+  */
+
   if (settings.text) {
     movie.scenes[0].elements.push({
       type: "text",
-      text: settings.text
+      text: settings.text,
+
+      settings:
+        settings.textSettings || {}
     });
   }
+
+
+  /*
+  |--------------------------------------------------------------------------
+  | Audio
+  |--------------------------------------------------------------------------
+  */
 
   if (settings.audioUrl) {
     movie.scenes[0].elements.push({
       type: "audio",
-      src: settings.audioUrl
+      src: settings.audioUrl,
+
+      volume:
+        settings.audioVolume ?? 1
+    });
+  }
+
+
+  /*
+  |--------------------------------------------------------------------------
+  | Subtitles
+  |--------------------------------------------------------------------------
+  */
+
+  if (settings.subtitles) {
+    if (!movie.elements) {
+      movie.elements = [];
+    }
+
+    movie.elements.push({
+      type: "subtitles",
+
+      ...(settings.subtitleLanguage
+        ? {
+            language:
+              settings.subtitleLanguage
+          }
+        : {}),
+
+      ...(settings.subtitleModel
+        ? {
+            model:
+              settings.subtitleModel
+          }
+        : {}),
+
+      ...(settings.subtitleSettings
+        ? {
+            settings:
+              settings.subtitleSettings
+          }
+        : {})
     });
   }
 
