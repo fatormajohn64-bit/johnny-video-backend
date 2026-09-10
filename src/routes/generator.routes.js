aa91d239a5356e62ee9a68d635fa58fd10fb45d5
Johnny-Tech <fatormajohn64@gmail.com>
2026-09-10 04:01:15 +0000

    Add route for generating Hugging Face content

--- src/routes/generator.routes.js
+++ src/routes/generator.routes.js
@@ -4,11 +4,20 @@ import {
   generateKling
 } from "../controllers/kling.controller.js";
 
+import {
+  generateHuggingFace
+} from "../controllers/huggingface.controller.js";
+
 const router = Router();
 
 router.post(
   "/kling/generate",
   generateKling
+);
+
+router.post(
+  "/huggingface/generate",
+  generateHuggingFace
 );
 
 export default router;
