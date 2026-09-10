5153f3056e9c31fff4333c3d6a27690c20cb3b87
Johnny-Tech <fatormajohn64@gmail.com>
2026-09-10 04:30:11 +0000

    Create settings.routes.js

---
+++ src/controllers/src/routes/settings.routes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+import {
+  getProviderSettings
+} from "../controllers/settings.controller.js";
+
+const router = Router();
+
+router.get(
+  "/:provider",
+  getProviderSettings
+);
+
+export default router;
