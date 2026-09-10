047627757ce3d85c262f3a02aff2968bf99ed26b
Johnny-Tech <fatormajohn64@gmail.com>
2026-09-10 13:04:46 +0000

    Create upload.middleware.js

---
+++ src/middleware/upload.middleware.js
@@ -0,0 +1,21 @@
+import multer from "multer";
+
+const storage = multer.memoryStorage();
+
+export const uploadVideo = multer({
+  storage,
+
+  limits: {
+    fileSize: 500 * 1024 * 1024
+  },
+
+  fileFilter(req, file, cb) {
+    if (!file.mimetype.startsWith("video/")) {
+      return cb(
+        new Error("Only video files are allowed")
+      );
+    }
+
+    cb(null, true);
+  }
+}).single("video");
